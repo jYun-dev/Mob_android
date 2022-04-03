bc79f174ccb541d814751b4724e151c5efb59b59
jeYun-dev <102717765+jYun-dev@users.noreply.github.com>
2022-04-03 16:46:42 +0900

    Add files via upload

---
+++ Practice/practice01/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobile_act01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
